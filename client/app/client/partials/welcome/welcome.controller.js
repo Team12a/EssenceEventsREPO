befbea1d36d7349aa7a2d4c033e3e5696751ac7b
rebeccaloredo <rebeccaloredo@users.noreply.github.com>
2017-10-04 23:40:34 -0400

    Welcome page now shows the events, still working on getting the overview to display on the same page.

---
+++ client/app/client/partials/welcome/welcome.controller.js
@@ -0,0 +1,35 @@
+'use strict';
+
+angular.module('essenceEventsRepoApp.client')
+.controller('WelcomeCtrl', [ 'Events', 'Auth', '$scope', '$stateParams', '$state', function ( Events, Auth, $scope, $stateParams, $state) {
+
+var getUser = function() {
+  if (!$scope.curUser._id)
+    setTimeout(getUser, 100);
+  else {
+    $scope.clientName = $scope.curUser.name;
+    $scope.id = $scope.curUser._id;
+  }
+};
+
+$scope.curUser = Auth.getCurrentUser();
+getUser();
+
+$scope.getEvents = function(){
+  if (!$scope.id)
+    setTimeout($scope.getEvents, 100);
+  else
+    Events.getByUser($scope.id)
+      .then(function(response) {
+        $scope.events = response.data;
+      }, function(error) {
+        //do something
+    });
+};
+
+$scope.navigate = function(eevent){
+
+  $state.go('myEvents' ,{eventt: eevent});
+
+}
+}]);
