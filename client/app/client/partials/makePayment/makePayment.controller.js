3bbe2dfe6568c877c73866b708474c526c8d8a2b
Althea <asmith790@ufl.edu>
2017-10-10 15:23:57 -0400

    Added getPayments() function

--- client/app/client/partials/makePayment/makePayment.controller.js
+++ client/app/client/partials/makePayment/makePayment.controller.js
@@ -1,6 +1,31 @@
-angular.module('essenceEventsRepoApp.client')
-  .controller('makePaymentCtrl', ['$scope', '$state', 'Payments', function ($scope, $state, Payments) {
+'use strict';
 
+angular.module('essenceEventsRepoApp.client')
+  .controller('makePaymentCtrl', ['$scope', '$state', 'Payments', 'Auth', function ($scope, $state, Payments, Auth) {
+
+    var getUser = function() {
+      if (!$scope.curUser._id)
+        setTimeout(getUser, 100);
+      else {
+        $scope.clientName = $scope.curUser.name;
+        $scope.id = $scope.curUser._id;
+      }
+    };
+
+    $scope.curUser = Auth.getCurrentUser();
+    getUser();
+
+    $scope.getPayments = function(){
+      if (!$scope.id)
+        setTimeout($scope.getPayments, 100);
+      else
+        Payments.paymentsByUserId($scope.id)
+          .then(function(response) {
+            $scope.payments = response.data;
+          }, function(error) {
+            //do something
+        });
+    };
 
 
     $scope.find = function() {
