dcf36aee754df3930f7a99b5137103cf1e590361
Althea <asmith790@ufl.edu>
2017-10-18 11:50:16 -0400

    Added in drop down menu with events

--- client/app/client/partials/manageGuestList/manageGuestList.controller.js
+++ client/app/client/partials/manageGuestList/manageGuestList.controller.js
@@ -1,8 +1,10 @@
 'use strict';
 
 angular.module('essenceEventsRepoApp.client')
-.controller('manageGuestListCtrl', [ 'Events', 'Auth', '$scope','$stateParams', '$state', function ( Events, Auth, $scope, $stateParams, $state) {
+.controller('manageGuestListCtrl', [ 'Events', 'Auth',  '$scope','$stateParams', '$state', function ( Events, Auth, $scope, $stateParams, $state) {
 
+
+$scope.ev = $scope.Selectedevent;
 
 var getUser = function() {
   if (!$scope.curUser._id)
@@ -32,5 +34,14 @@ $scope.getEvents = function(){
   {
     return (arr.length > 0);
   };
+
+$scope.getGuests = function(event){
+  if(!$scope.id)
+    setTimeout($scope.getGuests, 100);
+  else
+    $scope.ev = event;
+        //do something
+
+};
 
 }]);
