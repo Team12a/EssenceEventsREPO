6598a0483a0d397197bb5f42048b7e683988274d
rebeccaloredo <rebeccaloredo@users.noreply.github.com>
2017-11-28 17:53:54 -0500

    getting no where with the posting

--- server/routes.js
+++ server/routes.js
@@ -17,7 +17,7 @@ export default function(app) {
   app.use('/api/testimonials', require('./api/testimonial'));
   app.use('/api/payments', require('./api/payment'));
   app.use('/api/testimonial', require('./api/testimonial'));
-  
+
 
 
   app.use('/auth', require('./auth'));
