0448a42a1d88047106e9f77ae5a5766c1c499148
Zachary Pohl <zpohl97@ufl.edu>
2017-12-04 15:40:12 -0500

    broken cloudinary code, not sure how to fix

--- client/app/main/main.controller.js
+++ client/app/main/main.controller.js
@@ -49,53 +49,56 @@ class MainController {
 }
 })
 angular.module('essenceEventsRepoApp')
-  .controller('MainController',['Cloudinary', '$scope', '$stateParams', '$state', '$timeout', function (Cloudinary, $scope, $stateParams, $state, $timeout) {
-    
-    var actualUploadFile = function(file) {
-      var payload = Cloudinary.signFileUploadRequest();
-      payload.then(function(data) {
-        
-        var signature = data.data.signature;
-        var timestamp = data.data.timestamp;
-        var formData = new FormData(),
-          xhr = new XMLHttpRequest(),
-          cloudName = "dxqqlgb4t";
-        console.log(file);
-        formData.append("file", file);
-        formData.append("upload_preset", "kinnmurr");
-        formData.append("timestamp", timestamp);
-        formData.append("signature", signature); // REQUIRED
-        formData.append("invalidate",true);
-        formData.append("public_id","new_new_test");
-        for (var pair of formData.entries()) {
-          console.log(pair[0]+ ', ' + pair[1]); 
-        }
-        xhr.open("POST", "https://api.cloudinary.com/v1_1/" +
-          cloudName +
-          "/image/upload");
-        xhr.onload = () => {
-          if (xhr.status === 200) {
-          
-            // Success! You probably want to save the URL somewhere
-            var response = JSON.parse(xhr.response);
-            var temp = response.secure_url;
-            console.log(response);
-            // https address of uploaded file
-          } else {
-            alert("Upload failed. Please try agian.");
-          }
-        }
-        xhr.send(formData);
-
-      });
+.controller('MainController',['Cloudinary', '$scope', '$stateParams', '$state', '$timeout', function (Cloudinary, $scope, $stateParams, $state, $timeout) {
+  
+  var actualUploadFile = function(file) {
+    var payload = Cloudinary.signFileUploadRequest();//Generates signature and timestamp server side
+    payload.then(function(data) {
+      var signature = data.data.signature;
+      var timestamp = data.data.timestamp;
+      var formData = new FormData(),
+        xhr = new XMLHttpRequest(),
+        cloudName = "dxqqlgb4t";
+      console.log(file);
+      formData.append("file", file.name);
+      formData.append("api_key", "328814721185186");
+      formData.append("timestamp", timestamp);
+      formData.append("signature", signature);
+      formData.append("callback", "http://" + window.location.host + "/cloudinary_cors.html"); 
       
-    };
-    $scope.uploadFile = function(file) {
-      var file = document.getElementById(file).files[0];
-      $timeout( function(){
-        console.log(file);
-      },10000, true );
-      //actualUploadFile(file);
-    }
+      formData.append("invalidate",true);
+      formData.append("public_id","new_new_test");
+      for (var pair of formData.entries()) {
+        console.log(pair[0]+ ', ' + pair[1]); 
+      }
+      xhr.open("POST", "https://api.cloudinary.com/v1_1/" +
+        cloudName +
+        "/image/upload");
+      xhr.onload = () => {
+        if (xhr.status === 200) {
+        
+          // Success! You probably want to save the URL somewhere
+          console.warn(xhr.response)
+          //console.log(xhr.response);
+          var response = JSON.parse(xhr.response);
+          var temp = response.secure_url;
+          console.log(response);
+          // https address of uploaded file
+        } else {
+          alert("Upload failed. Please try agian.");
+        }
+      }
+      xhr.send(formData);
+
+    });
+    
+  };
+
+  $scope.uploadImage = function(files) {
+    var fileToSend = document.getElementById(files).files[0];
+    console.log(fileToSend.name);
+    actualUploadFile(fileToSend);
+  }
+ 
 }]);
 
--- server/routes.js
+++ server/routes.js
@@ -9,6 +9,12 @@ import path from 'path';
 
 export default function(app) {
   // Routes used
+  app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  });
+
   app.use('/api/users', require('./api/user'));
   app.use('/api/subcontractors', require('./api/subcontractor'));
   app.use('/api/events', require('./api/event'));
@@ -20,8 +26,12 @@ export default function(app) {
   app.use('/api/superAdmins', require('./api/superAdmin'));
   app.use('/api/textEdit', require('./api/textEdit'));
   app.use('/api/cloudinary', require('./api/cloudinary'));
+  app.use('/cloudinary_cors.html', require('./api/cloudinary/cloudinary_cors.html'));
 
   app.use('/auth', require('./auth'));
+
+  //Not a clue what the fuck this does!
+  
 
   // All undefined asset or api routes should return a 404
   app.route('/:url(api|auth|components|app|bower_components|assets)/*')
