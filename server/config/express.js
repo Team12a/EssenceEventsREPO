14e8a1ad20b2e92edb9354143642b023dccfd3bf
Noah Quinones <nquinone@ufl.edu>
2017-12-09 17:36:30 -0500

    Added some code to have bower_components loaded

--- server/config/express.js
+++ server/config/express.js
@@ -67,12 +67,11 @@ export default function(app) {
   }
 
   app.set('appPath', path.join(config.root, 'client'));
-  // app.set('bowerPath', path.join(conf.root,'bower_components'));
 
   if ('production' === env) {
     app.use(favicon(path.join(config.root, 'client', 'favicon.ico')));
     app.use(express.static(app.get('appPath')));
-    // app.use('/bower_components' ,express.static(app.get('bowerPath')));
+    app.use('/bower_components' , express.static(__dirname + '/bower_components'));
     app.use(morgan('dev'));
   }
 
